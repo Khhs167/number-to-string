f5635bfe1f7ff2027b2531f793707bbb54ec14a9
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2022-09-24 10:28:02 +0200

    Fixed #2

--- numtostring/numberToText.py
+++ numtostring/numberToText.py
@@ -10,7 +10,7 @@
 #This code is for numbers zero to one thousand.
 
 singulars = {0:"zero", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", "binder":" "} #This way we can cound how many ones, hundreds and thousands we have!
-teens = {10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"forteen", 15:"fifteen", 16:"sixteen", 17:"seventeen", 18:"eighteen", 19:"nineteen", "binder":" "} #We need them to check for any numbers between 10 and 19.
+teens = {10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen", 18:"eighteen", 19:"nineteen", "binder":" "} #We need them to check for any numbers between 10 and 19.
 tenths = {2:"twenty", 3:"thirty", 4:"forty", 5:"fifty", 6:"sixty", 7:"seventy", 8:"eighty", 9:"ninety", "binder":" "}#For anything between 20 to 99
 hundreds = {1:"one hundred", 2:"two hundred", 3:"three hundred", 4:"four hundred", 5:"five hundred", 6:"six hundred", 7:"seven hundred", 8:"eight hundred", 9:"nine hundred", "binder":" and "} #No need for anything more than usual
 #Binder is what binds the number with later numbers, can be useful.
